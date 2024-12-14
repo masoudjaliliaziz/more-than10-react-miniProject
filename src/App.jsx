b0715169c7364ee9c80486bd4e0ee25e7aac5d1d
masoudjaliliaziz <masoudjalili0781@gmail.com>
2024-12-14 13:09:20 +0330

    setup the branch

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import { useState } from "react";
 
 function App() {
-  return;
+  return (
+    <h1 className="bg-green-800 font-bold text-3xl text-white p-5">
+      hELLO FROM NEXT SESSION
+    </h1>
+  );
 }
 
 export default App;
