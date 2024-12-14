2392ff4bb13ac2270ba7c4c78e83e29634fac618
masoudjaliliaziz <masoudjalili0781@gmail.com>
2024-12-14 12:55:24 +0330

    complete the seprete the app to components and add some feature on it

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,32 @@
+import { useState } from "react";
+import Bill from "./components/Bill";
+import Tip from "./components/Tip";
 
-import { useState } from "react"
+import Calc from "./components/Calc";
+import Reset from "./components/Reset";
 function App() {
-  const[bill,setBill]=useState();
-  const[tip,setTip]=useState(0);
-  const[ftip,setFtip]=useState(0);
-  let percentage=Math.round((tip+ftip)/2);
-  let finalPay=(bill*percentage)/100;
-  console.log(percentage);
-return (
-    
+  const [bill, setBill] = useState();
+  const [tip, setTip] = useState(0);
+  const [ftip, setFtip] = useState(0);
+  let percentage = Math.round((tip + ftip) / 2);
+  let finalPay = (bill * percentage) / 100;
+  return (
     <div className=" mx-auto my-12 flex flex-col justify-center items-center gap-5 w-[30%]">
-     
-    <div>
-      <label htmlFor="">how much was the bill?</label>
-      <input className="bg-slate-100" type="text" value={bill} onChange={(e)=>{setBill(e.target.value)}} />
+      <Bill bill={bill} setBill={setBill} />
+      <Tip tip={tip} setTip={setTip}>
+        how did you like the service?
+      </Tip>
+      <Tip tip={ftip} setTip={setFtip}>
+        how did your friend like the service?
+      </Tip>
+      {bill > 0 && (
+        <>
+          <Calc bill={bill} finalPay={finalPay} />
+          <Reset onSetBill={setBill} onSetFtip={setFtip} OnSetTip={setTip} />
+        </>
+      )}
     </div>
-    <div>
-      <label htmlFor="">how did you like the service?</label>
-      <select name="" id="" value={tip} onChange={(e)=>{setTip(Number(e.target.value))}}>
-<option value="0">dissatisfied(0%)</option>
-<option value="5">it was okay(5%)</option>
-<option value="10">it was good(10%)</option>
-<option value="20">amazing(20%)</option>
-
-      </select>
-    </div>
-    <div>
-      <label htmlFor="">how did your friend like the service?</label>
-      <select name="" id="" value={ftip} onChange={(e)=>{setFtip(Number(e.target.value))}}>
-<option value="0">dissatisfied(0%)</option>
-<option value="5">it was okay(5%)</option>
-<option value="10">it was good(10%)</option>
-<option value="20">amazing(20%)</option>
-
-      </select>
-    </div>
-
-    <h1>ypu pay ${Number(bill)+Number(finalPay)} (${bill}+${finalPay})</h1>
-    <button className="bg-green-600 p-2 rounded-lg text-white " onClick={()=>{
-      setBill(" ");
-      setTip(0);
-      setFtip(0);
-    }}>reset</button>
-    </div>
-  )
+  );
 }
 
-export default App
+export default App;
