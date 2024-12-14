e02ec3d09727cb3cbb5ad35f85077547b58ec5de
masoudjaliliaziz <masoudjalili0781@gmail.com>
2024-12-14 16:08:09 +0330

    finish the static UI part of project

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,65 @@
+import { list } from "postcss";
 import { useState } from "react";
+
+const initialFriends = [
+  {
+    id: 118836,
+    name: "Clark",
+    image: "https://i.pravatar.cc/48?u=118836",
+    balance: -7,
+  },
+  {
+    id: 933372,
+    name: "Sarah",
+    image: "https://i.pravatar.cc/48?u=933372",
+    balance: 20,
+  },
+  {
+    id: 499476,
+    name: "Anthony",
+    image: "https://i.pravatar.cc/48?u=499476",
+    balance: 0,
+  },
+];
 
 function App() {
   return (
-    <h1 className="bg-green-800 font-bold text-3xl text-white p-5">
-      hELLO FROM NEXT SESSION
-    </h1>
+    <div className="app">
+      <div className="sidebar">
+        <FriendList />
+      </div>
+    </div>
   );
 }
-
+function FriendList() {
+  return (
+    <ul>
+      {initialFriends.map((friend) => {
+        return <Friend friend={friend} key={friend.id} />;
+      })}
+    </ul>
+  );
+}
+function Friend({ friend }) {
+  return (
+    <li>
+      <img src={friend.image} alt={friend.name} />
+      <h3>{friend.name}</h3>
+      {/* start conditional rendering--------------------- */}
+      {friend.balance < 0 && (
+        <p className="red">
+          you owe {friend.name} {-friend.balance}$
+        </p>
+      )}
+      {friend.balance == 0 && <p>you and {friend.name} are even</p>}
+      {friend.balance > 0 && (
+        <p className="green">
+          {friend.name} owe you {friend.balance}$
+        </p>
+      )}
+      {/* finish conditional rendering ----------------------*/}
+      <button className="button">select</button>
+    </li>
+  );
+}
 export default App;
