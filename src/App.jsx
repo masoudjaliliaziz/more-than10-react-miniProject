e81a184e31a15e04428e288f2a8702f67fa6e045
masoudjaliliaziz <masoudjalili0781@gmail.com>
2024-12-14 11:01:15 +0330

    finish bill count before seprate to the components

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
+import { useState } from "react"
 function App() {
-  const [count, setCount] = useState(0)
+  const[bill,setBill]=useState();
+  const[tip,setTip]=useState(0);
+  const[ftip,setFtip]=useState(0);
+  let percentage=Math.round((tip+ftip)/2);
+  let finalPay=(bill*percentage)/100;
+  console.log(percentage);
+return (
+    
+    <div className=" mx-auto my-12 flex flex-col justify-center items-center gap-5 w-[30%]">
+     
+    <div>
+      <label htmlFor="">how much was the bill?</label>
+      <input className="bg-slate-100" type="text" value={bill} onChange={(e)=>{setBill(e.target.value)}} />
+    </div>
+    <div>
+      <label htmlFor="">how did you like the service?</label>
+      <select name="" id="" value={tip} onChange={(e)=>{setTip(Number(e.target.value))}}>
+<option value="0">dissatisfied(0%)</option>
+<option value="5">it was okay(5%)</option>
+<option value="10">it was good(10%)</option>
+<option value="20">amazing(20%)</option>
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vitee + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      </select>
+    </div>
+    <div>
+      <label htmlFor="">how did your friend like the service?</label>
+      <select name="" id="" value={ftip} onChange={(e)=>{setFtip(Number(e.target.value))}}>
+<option value="0">dissatisfied(0%)</option>
+<option value="5">it was okay(5%)</option>
+<option value="10">it was good(10%)</option>
+<option value="20">amazing(20%)</option>
+
+      </select>
+    </div>
+
+    <h1>ypu pay ${Number(bill)+Number(finalPay)} (${bill}+${finalPay})</h1>
+    <button className="bg-green-600 p-2 rounded-lg text-white " onClick={()=>{
+      setBill(" ");
+      setTip(0);
+      setFtip(0);
+    }}>reset</button>
+    </div>
   )
 }
 
