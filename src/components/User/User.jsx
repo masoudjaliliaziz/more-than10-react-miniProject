6cec54b796c1dd33974058d4c74b74631e8d79ef
masoudjaliliaziz <masoudjalili0781@gmail.com>
2024-12-16 11:48:18 +0330

    complete the feture for adding user from form tko table

--- src/components/User/User.jsx
+++ src/components/User/User.jsx
@@ -1,17 +1,17 @@
-import React from "react";
-import UserTable from "./childComponents/UserTable";
-
-const users = [
-  { id: 1, name: "masoud", lastName: "jalili" },
-
-  { id: 2, name: "ali", lastName: "kianmehr" },
-  { id: 3, name: "mozhgan", lastName: "ragabi" },
-];
+import React, { useState } from "react";
+import UserTable from "./childComponents/userTable/UserTable";
+import UserForm from "./childComponents/userForm/UserForm";
 
 function User() {
+  const [users, setUsers] = useState([
+    { id: 1, name: "masoud", lastName: "jalili" },
+    { id: 2, name: "ali", lastName: "kianmehr" },
+    { id: 3, name: "mozhgan", lastName: "ragabi" },
+  ]);
   return (
     <div>
       <UserTable user={users} />
+      <UserForm users={users} setUsers={setUsers} />
     </div>
   );
 }
---
+++ src/components/User/childComponents/userForm/UserForm.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+
+function UserForm({ users, setUsers }) {
+  const [name, setName] = useState("");
+  const [lastName, setLastName] = useState("");
+  function handleSubmit(e) {
+    e.preventDefault();
+    const id = users.map((u) => u.id).length + 1;
+    setUsers((cur) => [...cur, { id, name, lastName }]);
+    setName("");
+    setLastName("");
+  }
+
+  return (
+    <form
+      className="w-7/12 flex flex-col mx-auto gap-3  "
+      action=""
+      onSubmit={handleSubmit}
+    >
+      <label htmlFor="name">insert the name</label>
+      <input
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        className="bg-slate-100 p-2 rounded-lg"
+        type="text"
+        id="name"
+      />
+      <label htmlFor="lastName">insert the last name</label>
+      <input
+        value={lastName}
+        onChange={(e) => setLastName(e.target.value)}
+        className="bg-slate-100 p-2 rounded-lg"
+        type="text"
+        id="lastName"
+      />
+      <button
+        className="bg-green-700 text-white p-2 rounded-lg font-bold text-lg"
+        type="submit"
+      >
+        insert
+      </button>
+    </form>
+  );
+}
+
+export default UserForm;
