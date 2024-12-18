93eb1e2a4cd578cb7ce818644d58b5a22a940031
masoudjaliliaziz <masoudjalili0781@gmail.com>
2024-12-18 12:28:36 +0330

    setup the repo for new mini app

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,9 @@
 import { list } from "postcss";
 import { useState } from "react";
-import ShowMore from "./components/showMore/ShowMore";
+import HowReactWork from "./components/how react work/HowReactWork";
 
 function App() {
-  return (
-    <ShowMore>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut similique
-      aliquam harum reiciendis consectetur, sit ad rerum, saepe alias quis
-      voluptatem ipsum itaque nam. Ipsa sapiente cumque error facere debitis
-      nemo magnam, dignissimos doloribus possimus repudiandae voluptate delectus
-      atque nisi provident iusto autem, amet fugit cupiditate quidem tempora
-      dicta et.
-    </ShowMore>
-  );
+  return <HowReactWork />;
 }
 
 export default App;
