b61c996db9002ed5d5aa875717b04eb20cdec5ae
masoudjaliliaziz <masoudjalili0781@gmail.com>
2024-12-21 13:37:35 +0330

    finish the section about fething data

--- src/components/CurrencyConverter/Currency.jsx
+++ src/components/CurrencyConverter/Currency.jsx
@@ -4,19 +4,23 @@ function Currency() {
   const [from, setFrom] = useState("USD");
   const [to, setTo] = useState("EUR");
   const [amount, setAmount] = useState(1);
-  const [output, setOutput] = useState(1);
+  const [output, setOutput] = useState("");
   const [calcaluating, setCalcaluating] = useState(false);
   useEffect(
     function () {
-      setCalcaluating(true);
       async function convert() {
+        setCalcaluating(true);
         const res = await fetch(
           `https://api.frankfurter.app/latest?amount=${amount}&from=${from}&to=${to}`
         );
         const data = await res.json();
         setOutput(data.rates[to]);
+        setCalcaluating(false);
       }
-      setCalcaluating(false);
+      if (from === to) return setOutput(amount);
+      if (amount === 0 || amount === " ") {
+        return setCalcaluating(false);
+      }
       convert();
     },
     [amount, from, to]
@@ -26,14 +30,16 @@ function Currency() {
       <div className="flex gap-2">
         <input
           value={amount}
-          onChange={(e) => setAmount(e.target.value)}
+          onChange={(e) => setAmount(Number(e.target.value))}
           type="text"
           className="rounded-md outline-blue-300 px-4 text-blue-900 font-bold"
+          disabled={calcaluating}
         />
         <select
           value={from}
           onChange={(e) => setFrom(e.target.value)}
           className="w-1/8 p-2 bg-blue-500 text-white font-bold rounded-md outline-none"
+          disabled={calcaluating}
         >
           <option name="" id="" value="USD">
             USD
@@ -52,6 +58,7 @@ function Currency() {
           value={to}
           onChange={(e) => setTo(e.target.value)}
           className="w-1/8 p-2 bg-green-500 text-white font-bold rounded-md outline-none"
+          disabled={calcaluating}
         >
           <option name="" id="" value="USD">
             USD
