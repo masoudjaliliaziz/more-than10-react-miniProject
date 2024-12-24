457f53b4c4ef724ef0c163629d12cab03f91b4c4
masoudjaliliaziz <masoudjalili0781@gmail.com>
2024-12-24 22:10:29 +0330

    setup the project useReducer

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,10 @@ import { list } from "postcss";
 import { useState } from "react";
 import Currency from "./components/CurrencyConverter/Currency";
 import Geo from "./components/customhooksChallange/Geo";
+import DateCounter from "./components/UseReducer/DateCounter";
 
-function App() {}
+function App() {
+  return <DateCounter />;
+}
 
 export default App;
