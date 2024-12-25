2ab5ccb2da9adbcd0a793cf3133de2c9e7e1d5dc
masoudjaliliaziz <masoudjalili0781@gmail.com>
2024-12-25 12:37:07 +0330

    conditional rendering for each status of data fething in useReducer

--- src/components/UseReducer/Question.jsx
+++ src/components/UseReducer/Question.jsx
@@ -2,6 +2,10 @@ import React, { useEffect, useReducer } from "react";
 import Header from "./childComponents/Header";
 import Main from "./childComponents/Main";
 import { data } from "autoprefixer";
+import loader from "./childComponents/Loader";
+import Error from "./childComponents/Error";
+import StartScreen from "./childComponents/StartScreen";
+import Loader from "./childComponents/Loader";
 
 const initialState = {
   questions: [],
@@ -19,21 +23,23 @@ function reducer(state, action) {
 function Question() {
   const [state, dispatch] = useReducer(reducer, initialState);
   const { status, questions } = state;
-  console.log(status);
-  4;
-  console.log(questions);
-
   useEffect(function () {
     fetch("http://localhost:8000/questions")
       .then((res) => res.json())
       .then((data) => dispatch({ type: "dataRecived", payload: data }))
       .catch((err) => dispatch({ type: "dataFailed" }));
   }, []);
+  const questionNum = questions.length;
+  console.log(status);
+  console.log(questions);
   return (
     <div className="app">
       <Header />
       <Main>
-        <p>question 1/15</p>
+        {status === "loadiong" && <Loader />}
+
+        {status === "error" && <Error />}
+        {status === "ready" && <StartScreen questionNum={questionNum} />}
       </Main>
     </div>
   );
