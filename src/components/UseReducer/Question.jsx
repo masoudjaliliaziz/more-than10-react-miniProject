920689fa97923cc7d7404817b05eda9242a9b043
masoudjaliliaziz <masoudjalili0781@gmail.com>
2024-12-25 12:15:31 +0330

    setup the use reducer and pass the state of status and questions and handle it when status is ready , loadiung , error

--- src/components/UseReducer/Question.jsx
+++ src/components/UseReducer/Question.jsx
@@ -1,7 +1,34 @@
-import React from "react";
+import React, { useEffect, useReducer } from "react";
 import Header from "./childComponents/Header";
 import Main from "./childComponents/Main";
+import { data } from "autoprefixer";
+
+const initialState = {
+  questions: [],
+  status: "loading",
+};
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataRecived":
+      return { ...state, questions: action.payload, status: "ready" };
+    case "dataFailed":
+      return { ...state, status: "error" };
+  }
+}
+
 function Question() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const { status, questions } = state;
+  console.log(status);
+  4;
+  console.log(questions);
+
+  useEffect(function () {
+    fetch("http://localhost:8000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataRecived", payload: data }))
+      .catch((err) => dispatch({ type: "dataFailed" }));
+  }, []);
   return (
     <div className="app">
       <Header />
