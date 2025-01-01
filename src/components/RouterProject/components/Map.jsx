9423820abc25d7b4f9a1297d0513d90f598444ad
masoudjaliliaziz <masoudjalili0781@gmail.com>
2025-01-01 22:46:16 +0330

    adding leaflet library to projrct and shown the city marker on the map

--- src/components/RouterProject/components/Map.jsx
+++ src/components/RouterProject/components/Map.jsx
@@ -1,25 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Map.module.css";
 import { useNavigate, useSearchParams } from "react-router-dom";
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import { useCities } from "../contexts/CitiesContext";
 
 function Map() {
-  const navigate = useNavigate();
+  const { cities } = useCities();
   const [searchParam, setSearchParams] = useSearchParams();
-  const lat = searchParam.get("lat");
-  const lng = searchParam.get("lng");
+  // const lat = searchParam.get("lat");
+  // const lng = searchParam.get("lng");
+  const [mapPosition, setMapPosition] = useState([40, 40]);
   return (
-    <div className={styles.mapContainer} onClick={() => navigate("form")}>
-      <h1>Map</h1>
-      <p>
-        position: {lat} , {lng}
-      </p>
-      <button
-        onClick={() => {
-          setSearchParams({ lat: 20, lng: 40 });
-        }}
+    <div className={styles.mapContainer}>
+      <MapContainer
+        className={styles.map}
+        center={mapPosition}
+        zoom={5}
+        scrollWheelZoom={true}
       >
-        change position
-      </button>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
+        />
+        {cities.map((city) => (
+          <Marker
+            position={[city.position.lat, city.position.lng]}
+            key={city.id}
+          >
+            <Popup>
+              <span>{city.emoji}</span> <span>{city.cityName}</span>
+            </Popup>
+          </Marker>
+        ))}
+      </MapContainer>
     </div>
   );
 }
