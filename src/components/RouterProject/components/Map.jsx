58d7401dd5f8ce848561ffa6ea80b913c3e62c9a
masoudjaliliaziz <masoudjalili0781@gmail.com>
2025-01-01 23:25:34 +0330

    adding the feature that when we click in the area in map that navigate us to the form components and give us the lat and lng of clicked area on the url parametrs with react router

--- src/components/RouterProject/components/Map.jsx
+++ src/components/RouterProject/components/Map.jsx
@@ -1,7 +1,14 @@
 import React, { useEffect, useState } from "react";
 import styles from "./Map.module.css";
-import { useSearchParams } from "react-router-dom";
-import { MapContainer, Marker, Popup, TileLayer, useMap } from "react-leaflet";
+import { useNavigate, useSearchParams } from "react-router-dom";
+import {
+  MapContainer,
+  Marker,
+  Popup,
+  TileLayer,
+  useMap,
+  useMapEvents,
+} from "react-leaflet";
 import { useCities } from "../contexts/CitiesContext";
 import { map } from "leaflet";
 
@@ -11,7 +18,9 @@ function Map() {
   const mapLat = searchParam.get("lat");
   const mapLng = searchParam.get("lng");
 
-  const [mapPosition, setMapPosition] = useState([40, 40]);
+  const [mapPosition, setMapPosition] = useState([
+    35.401718382060736, 49.031097595824775,
+  ]);
   useEffect(() => {
     if (mapLat && mapLng) setMapPosition([mapLat, mapLng]);
   }, [mapLat, mapLng]);
@@ -20,7 +29,7 @@ function Map() {
       <MapContainer
         className={styles.map}
         center={mapPosition}
-        zoom={8}
+        zoom={13}
         scrollWheelZoom={true}
       >
         <TileLayer
@@ -38,13 +47,20 @@ function Map() {
           </Marker>
         ))}
         <ChangeCenter position={mapPosition} />
+        <DetectClick />
       </MapContainer>
     </div>
   );
 }
 function ChangeCenter({ position }) {
   const map = useMap();
-  map.setView(position);
+  map.setView(position, 6);
   return null;
+}
+function DetectClick() {
+  const navigate = useNavigate();
+  useMapEvents({
+    click: (e) => navigate(`form?lat=${e.latlng.lat}&lng=${e.latlng.lng}`),
+  });
 }
 export default Map;
